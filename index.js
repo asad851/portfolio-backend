257b86171a6e02c1ca967c69427a7bb8131e0157
asad851 <siddiqui851@gmail.com>
2025-08-25 14:01:56 +0530

    change resume

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Directory where your PDFs are stored
-const PDF_FILE_PATH = path.join(__dirname, "pdfs", "AsadResume.pdf");
+const PDF_FILE_PATH = path.join(__dirname, "pdfs", "asad_siddiqui.pdf");
 app.post("/send-email", async (req, res) => {
   try {
     let { firstName, lastName, email, phone, message } = req.body;
@@ -100,7 +100,7 @@ app.post("/send-email", async (req, res) => {
 });
 
 app.get("/download", (req, res) => {
-  res.download(PDF_FILE_PATH, "AsadResume.pdf", (err) => {
+  res.download(PDF_FILE_PATH, "asad_siddiqui.pdf", (err) => {
     if (err) {
       console.error("Error downloading file:", err);
       res.status(500).json({ error: "Failed to download file" });
